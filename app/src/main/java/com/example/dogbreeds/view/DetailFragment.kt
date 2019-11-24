1e6f12956e557efefb725ef8a1331dd8dd732d26
MN <michalnow98@interia.pl>
2019-11-24 07:37:38 +0100

    Populated fragments, implemented navigation between them with butons.

--- app/src/main/java/com/example/dogbreeds/view/DetailFragment.kt
+++ app/src/main/java/com/example/dogbreeds/view/DetailFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.example.dogbreeds.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 
 class DetailFragment : Fragment() {
@@ -20,5 +22,12 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        buttonList.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/example/dogbreeds/view/ListFragment.kt
+++ app/src/main/java/com/example/dogbreeds/view/ListFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import com.example.dogbreeds.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
@@ -20,5 +23,12 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        buttonDetails.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
