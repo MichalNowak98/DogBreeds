bde8b2bc5fa85cb8d7e3700c35242fa162036dfc
MN <michalnow98@interia.pl>
2019-11-30 21:43:22 +0100

    Auto stash before revert of "Created BaseViewModel, data is now stored in database"

--- app/src/main/java/com/example/dogbreeds/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/dogbreeds/viewmodel/BaseViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.dogbreeds.viewmodel
 
-class BaseViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+abstract class BaseViewModel(application: Application): AndroidViewModel(application), CoroutineScope {
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main  //after finishing job return to main thread
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
 }
--- app/src/main/java/com/example/dogbreeds/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/dogbreeds/viewmodel/ListViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.dogbreeds.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.dogbreeds.model.DogBreed
+import com.example.dogbreeds.model.DogDatabase
 import com.example.dogbreeds.model.DogsApiService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class ListViewModel: ViewModel() {
+class ListViewModel(application: Application): BaseViewModel(application) {
 
     private val dogsService = DogsApiService()
     private val disposable = CompositeDisposable() //handles memory leaks
@@ -30,9 +33,7 @@ class ListViewModel: ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())  //result processed in main thread
                 .subscribeWith(object: DisposableSingleObserver<List<DogBreed>>() {
                     override fun onSuccess(dogList: List<DogBreed>) {
-                        dogs.value = dogList
-                        dogsLoadError.value = false
-                        loading.value = false
+                        storeDogsLocally(dogList)
                     }
                     override fun onError(e: Throwable) {
                         dogsLoadError.value = true
@@ -41,6 +42,26 @@ class ListViewModel: ViewModel() {
                     }
                 })
         )
+    }
+
+    private fun dogsRetreived(dogList: List<DogBreed>) {
+        dogs.value = dogList
+        dogsLoadError.value = false
+        loading.value = false
+    }
+
+    private fun storeDogsLocally(list: List<DogBreed>){
+        launch {
+            val dao = DogDatabase(getApplication()).dogDao()
+            DogDatabase(getApplication()).dogDao().deleteAllDogs()
+            val result = dao.insertAll(*list.toTypedArray())
+            var i = 0
+            while (i < list.size) {
+                list[i].uuid = result[i].toInt()
+                i++
+            }
+            dogsRetreived(list)
+        }
     }
 
     override fun onCleared() {
